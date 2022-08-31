c7aa87eafe2d64fbc90168809c5abe67b6616982
Sophie <sophie@millionroads.com>
2022-08-31 08:53:06 +0200

    Begin aiohttp version of app (WIP)

--- chatbot_app/app.py
+++ chatbot_app/app.py
@@ -1,142 +1,34 @@
-import logging
-from uuid import uuid4
-from bot.elements import Elements
-from luis_tools.luis_functions import understand
-from entities_and_intents import *
 
-logging.basicConfig(
-    filename="./conversations.log", 
-    datefmt="%d/%m/%y %H:%M:%S",
-    format="%(asctime)s: %(levelname)s: %(message)s",
-    level=logging.INFO)
-
-FIRST_MESSAGE = 'first_message'
-START = 'start'
-
-def summary(elements):
-    return ("Let's sum up: you want to book a flight " 
-            f"from {getattr(elements, OR_CITY_ENTITY)} "
-            f"to {getattr(elements, DST_CITY_ENTITY)}, "
-            f"leaving on {getattr(elements, STR_DATE_ENTITY)} "
-            f"and returning on {getattr(elements, END_DATE_ENTITY)},"
-            f" for a budget of {getattr(elements, BUDGET_ENTITY)}?")
-
-class Dialog:
-    def __init__(self):
-        self.uuid = uuid4()
-        self.messages = {
-            FIRST_MESSAGE: "Hi, I'm your flight assistant.",
-            START: "How can I help you?",
-            NONE_INTENT: "I'm sorry, I didn't understand. Could you rephrase please?",
-            GREETING_INTENT: "I'm at your service!",
-            AGREE_INTENT: "Great, let's find your flights!",
-            DISAGREE_INTENT: "Sorry, I'm trying to do my best. Thanks for your patience!",
-            STR_DATE_ENTITY: "What is your departure date?",
-            END_DATE_ENTITY: "What is your return date?",
-            DST_CITY_ENTITY: "Where do you want to fly to?",
-            OR_CITY_ENTITY: "Where do you want to depart from?",
-            BUDGET_ENTITY: "What is your budget?", 
-            }
-
-    def _fix_end_date(self, elements, entities):
-        """
-        If str_date already exist among elements,
-        converts entities[STR_DATE_ENTITY] to entities[END_DATE_ENTITY].
-        """
-        if getattr(elements, STR_DATE_ENTITY)!='unknown' and STR_DATE_ENTITY in entities:
-            end_date = entities.pop(STR_DATE_ENTITY)
-            entities[END_DATE_ENTITY] = end_date
-        return entities
-
-    def _update_elements(self, elements: Elements, entities: dict) -> Elements:
-        """
-        Return elements updated with entities. 
-        """
-        for key, value in entities.items():
-            setattr(elements, key, value)
-        return elements
+from dotenv import dotenv_values
+from types import SimpleNamespace
+from aiohttp import web
+from aiohttp.web import Request, Response, json_response
+from http import HTTPStatus
 
 
-    def ask_till_all_elements_are_known(self, elements):
-        """
-        Ask for missing elements until all keys of elements
-        have a value (different from 'unknown').
-        """
-        topic = START
-        while not elements.is_complete():
-            if not topic in self.messages:
-                logging.error(f'Unknown topic: {topic}')
-                topic = NONE_INTENT
-            logging.info(f"{self.uuid}: elements = {elements.elements}")    
-            message = self.messages[topic]
-            logging.info(f"{self.uuid} BOT: {message}")
+config = SimpleNamespace(**dotenv_values())
 
-            text = input(message + '\n')
-            if not text:
-                topic = NONE_INTENT
-                continue
-
-            logging.info(f"{self.uuid} USER: {text}")
-            luis_response = understand(text)
-            intent, entities = luis_response['intent'], luis_response['entities']
-            if entities:
-                entities = self._fix_end_date(elements, entities)
-                elements = self._update_elements(elements, entities)
-                topic = elements.next_unknown_element()
-            else:
-                topic = intent if intent != 'inform' else NONE_INTENT
-        return elements
-
-    def ask_for_confirmation(self, elements):
-        """
-        Ask for confirmation of all gathered data about the flight.
-        Return the text typed by customer.
-        """
-        message = summary(elements)
-        print(message)
-        logging.info(f"{self.uuid} BOT: {message}")
-        return input()
-   
-    
-    def main(self,elements):
-        """
-        Main dialog loop.
-        """
-        first_message=True
-        while True:
-            if first_message:
-                print(self.messages[FIRST_MESSAGE], end=' ')
-                logging.info(f"{self.uuid} BOT: {self.messages[FIRST_MESSAGE]}")
-
-            elements = self.ask_till_all_elements_are_known(elements)
-            confirmation = self.ask_for_confirmation(elements)
-            logging.info(f"{self.uuid} USER: {confirmation}")
-
-            final_intent = understand(confirmation)['intent']
-
-            if final_intent == 'agree':
-                message = self.messages[AGREE_INTENT]
-                logging.info(f"{self.uuid} intent = {final_intent} *** SUCCESS***")
-                print(message)
-                logging.info(f"{self.uuid} BOT: {message}")
-                break
-
-            elif final_intent == 'disagree':
-                message = self.messages[DISAGREE_INTENT]
-                logging.warning(f"{self.uuid} intent = {final_intent} *** FAIL***")
-                print(message)
-                logging.info(f"{self.uuid} BOT: {message}")
-                elements.reset_values()
-                first_message = False
-
-            else:
-                message = self.messages[NONE_INTENT]
-                logging.info(f"{self.uuid} intent = {final_intent}")
-                logging.info(f"{self.uuid} BOT: {message}")
+async def messages(req: Request) -> Response:
+    """
+    Main bot message handler.
+    """
+    if "application/json" in req.headers["Content-Type"]:
+        # response = await req.json()
+        return Response(text="OK!", status=HTTPStatus.OK)
+    else:
+        return Response(status=HTTPStatus.UNSUPPORTED_MEDIA_TYPE)
 
 
-if __name__ == '__main__':
-    elements = Elements()
-    dialog = Dialog()
-    dialog.main(elements)
 
+async def health(req: Request) -> Response:
+    """
+    Check endpoint health.
+    """
+    return web.Response(text="App is running")
+
+chatbot = web.Application()
+chatbot.router.add_post("/api/messages", messages)
+chatbot.router.add_get("/api/messages", health)
+
+if __name__ == "__main__":
+    web.run_app(chatbot, port=config.bot_port)
