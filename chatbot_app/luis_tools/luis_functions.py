006567605aa3d1901d21e2444ac49529e15491a6
Sophie <sophiecrespo09@gmail.com>
2022-09-01 17:51:01 +0200

    Change output type hint

--- chatbot_app/luis_tools/luis_functions.py
+++ chatbot_app/luis_tools/luis_functions.py
@@ -7,9 +7,9 @@ from .luis_prediction import LuisPrediction
 luis_params = {key: value for key, value in dotenv_values().items() if key.startswith('luis_')}
 lm = LuisManager(**luis_params)
 
-def understand(text, prediction_func=lm.get_prediction_response) -> dict:
+def understand(text, prediction_func=lm.get_prediction_response) -> LuisPrediction:
     """
-    Return LUIS intent and entities for a given text.
+    Return LuisPrediction for a given text.
     """
    
     result = prediction_func(text)
