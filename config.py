a4c18d37ced8708fd3f254549d81a0832fc60ed1
Sophie <sophie@millionroads.com>
2022-09-18 11:23:36 +0200

    Add bot config to gitignore

--- config.py
+++
@@ -1,4 +0,0 @@
-MicrosoftAppType="MultiTenant"
-MicrosoftAppId="bd2e659f-59bd-499c-8f6b-b70000f4eb25" # The bot's app ID.
-MicrosoftAppPassword="ENM8Q~OXmn_sBsuAAICvCNHTeDHE7xOEGKK1YcoE" # The bot's app password (Secret Value)
-
